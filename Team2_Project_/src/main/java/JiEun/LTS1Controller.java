ac6123c1823c97b76158e17b775987a950f2e77b
pciio1957 <pciio1957@gmail.com>
2021-06-14 21:59:49 +0900

    0614 22:00 지은 패키지 생성

---
+++ Team2_Project_/src/main/java/JiEun/LTS1Controller.java
@@ -0,0 +1,5 @@
+package JiEun;
+
+public class LTS1Controller {
+
+}
---
+++ Team2_Project_/src/main/java/JiEun/LTS1DAO.java
@@ -0,0 +1,10 @@
+package JiEun;
+
+public class LTS1DAO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
