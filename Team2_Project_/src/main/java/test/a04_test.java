7f5d9a192f94882f7efc3f3ca3213955e13779dd
LDJ <LDJ@DESKTOP-PBU4M1N>
2021-06-14 21:48:51 +0900

    (덕재) 테스트2 06-14 21:48

--- Team2_Project_/src/main/java/test/a04_test.java
+++ Team2_Project_/src/main/java/test/a04_test.java
@@ -1,10 +1,11 @@
 package test;
 
-public class a03_test {
+public class a04_test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");
+		System.out.println("tt");
+
 	}
 
 }
