69047def6cee105b5fb676802ae0f6632a782db8
user <user@DESKTOP-COJ9858>
2021-06-14 18:54:56 +0900

    성근 18:53PM

--- Team2_Project_/src/main/java/test/a02_sksk.java
+++ Team2_Project_/src/main/java/test/a02_sksk.java
@@ -5,7 +5,7 @@ public class a02_sksk {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("성근 devlop 테스트");
+		System.out.println("성근 devlop 테스트!!!");
 
 	}
 
