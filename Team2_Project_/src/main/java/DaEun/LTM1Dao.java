543997a661bd97f37270b1b2f91abd8e9f7d4d03
A <A@DESKTOP-HQT62B7>
2021-06-16 08:52:26 +0900

    임시

--- Team2_Project_/src/main/java/DaEun/LTM1Dao.java
+++ Team2_Project_/src/main/java/DaEun/LTM1Dao.java
@@ -1,5 +1,13 @@
 package DaEun;
 
 public class LTM1Dao {
-
+	public String login(LTM1Member lm) {
+		System.out.println("# DB 처리(로그인) #");
+		System.out.println(lm.getId());
+		System.out.println(lm.getPass());
+		return lm.getId().equals("himan")&&
+				lm.getPass().equals("7777")?
+				"성공":"실패";
+		
+	}
 }
--- Team2_Project_/src/main/java/DaEun/LTM1Main.java
+++ Team2_Project_/src/main/java/DaEun/LTM1Main.java
@@ -1,10 +1,12 @@
 package DaEun;
 
+
 public class LTM1Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+			LTM1Controller c = new LTM1Controller();
+			c.login(new LTM1Member(), new LTM1Model());
 	}
 
 }
