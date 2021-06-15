4aefd725fabbafebb1abd2f9b4a7f5c6fb1e7873
A <A@DESKTOP-HQT62B7>
2021-06-16 08:59:41 +0900

    .

--- Team2_Project_/src/main/java/DaEun/LTM1Model.java
+++ Team2_Project_/src/main/java/DaEun/LTM1Model.java
@@ -1,7 +1,7 @@
 package DaEun;
 
 public class LTM1Model {
-	public void addAttribute(String key, Object ob) {
+	public void addAB(String key, Object ob) {
 		System.out.println("여기는 모델");
 		System.out.println("key:"+key);
 		System.out.println("객체:"+ob);
---
+++ Team2_Project_/src/main/java/DaEun/LTM2Controller.java
@@ -0,0 +1,5 @@
+package DaEun;
+// 인증 서비스
+public class LTM2Controller {
+
+}
