2622b4b815972cbd02ea47e16956112e7029a76a
A <A@DESKTOP-HQT62B7>
2021-06-16 09:01:24 +0900

    .

--- Team2_Project_/src/main/java/DaEun/LTM2Controller.java
+++ Team2_Project_/src/main/java/DaEun/LTM2Controller.java
@@ -1,5 +1,27 @@
 package DaEun;
 // 인증 서비스
 public class LTM2Controller {
+	
+	    private LTM2MemberService memberService;
+	    private MailSendService mss;
 
+
+	    @RequestMapping("/member/signUp")
+	     public void signUp(@ModelAttribute MemberDTO memberDTO){
+	        // DB에 기본정보 insert
+	        memberService.signUp(memberDTO);
+
+	        //임의의 authKey 생성 & 이메일 발송
+	        String authKey = mss.sendAuthMail(memberDTO.getEmail());
+	        memberDTO.setAuthKey(authKey);
+
+	        Map<String, String> map = new HashMap<String, String>();
+	        map.put("email", memberDTO.getEmail());
+	        map.put("authKey", memberDTO.getAuthKey());
+	        System.out.println(map);
+
+	      //DB에 authKey 업데이트
+	      memberService.updateAuthKey(map);
+
+	  	}
 }
