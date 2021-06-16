1d1ad612a5aa06a2aa765cfc1b7ed14871d9e72c
A <A@DESKTOP-HQT62B7>
2021-06-16 09:10:06 +0900

    6자리 생성 시스템

--- Team2_Project_/src/main/java/DaEun/LTM2MemberService.java
+++ Team2_Project_/src/main/java/DaEun/LTM2MemberService.java
@@ -1,54 +1,21 @@
 package DaEun;
+// 일단 휴대폰 인증 시스템을 못 만들어서 '만약 휴대폰 인증이 된다'는
+// 가정하에 만든 6자리 숫자 만들어지는 것
+import java.util.concurrent.ThreadLocalRandom;
 
 public class LTM2MemberService {
-	    private JavaMailSenderImpl mailSender;
-
-	    //인증키 생성
-	    private String getKey(int size) {
-	        this.size = size;
-	        return getAuthCode();
-	    }
-
-	    //인증코드 난수 발생
-	    private String getAuthCode() {
-	        Random random = new Random();
-	        StringBuffer buffer = new StringBuffer();
-	        int num = 0;
-
-	        while(buffer.length() < size) {
-	            num = random.nextInt(10);
-	            buffer.append(num);
-	        }
-
-	        return buffer.toString();
-	    }
-
-	    //인증메일 보내기
-	    public String sendAuthMail(String email) {
-	        //6자리 난수 인증번호 생성
-	        String authKey = getKey(6);
-
-	        //인증메일 보내기
-	        try {
-	            MailUtils sendMail = new MailUtils(mailSender);
-	            sendMail.setSubject("회원가입 이메일 인증");
-	            sendMail.setText(new StringBuffer().append("<h1>[이메일 인증]</h1>")
-	            .append("<p>아래 링크를 클릭하시면 이메일 인증이 완료됩니다.</p>")
-	            .append("<a href='http://localhost:9080/member/signUpConfirm?email=")
-	            .append(email)
-	            .append("&authKey=")
-	            .append(authKey)
-	            .append("' target='_blenk'>이메일 인증 확인</a>")
-	            .toString());
-	            sendMail.setFrom("이메일 주소", "관리자");
-	            sendMail.setTo(email);
-	            sendMail.send();
-	        } catch (MessagingException e) {
-	            e.printStackTrace();
-	        } catch (UnsupportedEncodingException e) {
-	            e.printStackTrace();
-	        }
-
-	          return authKey;
-	    }
+	public static int ltm2generateAuthNo1() {
+		 return ThreadLocalRandom.current().nextInt(100000, 1000000);
+		 
+	}
+	public static int ltm2generateAuthNo2() {
+        java.util.Random generator = new java.util.Random();
+        generator.setSeed(System.currentTimeMillis());
+        return generator.nextInt(1000000) % 1000000;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(ltm2generateAuthNo1());
+        System.out.println(ltm2generateAuthNo2());
+    }
 }
