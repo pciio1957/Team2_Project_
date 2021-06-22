1a77cc5a6252fa18918650052ec40afd8f6ed04f
heyjinss <heyjinss@naver.com>
2021-06-22 17:11:16 +0900

    혜진 (수정) 06.22 17:11 서비스단

--- Team2_Project_/src/main/java/Hyejin_LO1/LO1_Service.java
+++ Team2_Project_/src/main/java/Hyejin_LO1/LO1_Service.java
@@ -17,11 +17,16 @@ public class LO1_Service {
 		ArrayList<LO1_List> winlist = new ArrayList<LO1_List>();
 		
 		System.out.println("# 검색리스트 #");
-		System.out.println(" - " + s + ": 검색 결과 ");
+		System.out.println(" - " +"["+ s.getNum01() +"]"+"["+s.num02+"]"+"["+s.num03+"]"+
+				"["+s.num04+"]"+"["+s.num05+"]"+"["+s.num06+"]"+": 검색 결과 ");
+		
 		for(LO1_List l:winlist) {
 			// 사용자가 입력한 6자리 입력값이 [당첨번호]에 있으면 출
-			if(l.wincode.contains(s)) {
+			if(l.wincode.contains(s.getNum01())||l.wincode.contains(s.getNum02())
+					||l.wincode.contains(s.getNum03())||l.wincode.contains(s.getNum04())
+					||l.wincode.contains(s.getNum05())||l.wincode.contains(s.getNum06())) {
 				l.showInfo(s);
+			
 			
 			
 		
@@ -29,15 +34,16 @@ public class LO1_Service {
 		System.out.println("#서비스단처리#");
 		System.out.println("DAO에 전송 데이터: " + s.getNum01());
 		//System.out.println("DAO에서 받은 크기:" + dao.list(s).size());//
-		
+			
 	
-		
-	return dao.list(s);
+
 	
-				}
+				
 			}
+	
 		}
+		return dao.list(s);
 	}
 
-
+}
 	
