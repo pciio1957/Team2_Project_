4c9ea14bae237268ef5b25781a727c13af5acfe7
user <user@DESKTOP-COJ9858>
2021-06-28 09:04:34 +0900

    (성근) 06.28 09:05AM - JSCMain 수정 중

--- Team2_Project_/src/main/java/SungKeun/JSTUDY/JSC_COMMUNITY/JSCMain.java
+++ Team2_Project_/src/main/java/SungKeun/JSTUDY/JSC_COMMUNITY/JSCMain.java
@@ -19,8 +19,8 @@ public class JSCMain {
 		String writer; // 작성자
 		String headLine; // 게시물 제목
 		String write; // 게시물 작성 내용
-		Calendar cal = Calendar.getInstance(); // 작성일
-		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Calendar cal = Calendar.getInstance();
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss"); // 작성일
 //		System.out.println("프로그램 구동 시간 : " + dateFormat.format(cal.getTime()));
 		
 		// JSTUDY 커뮤니티 - 메인 메뉴
@@ -41,14 +41,13 @@ public class JSCMain {
             freeMenu = sc.nextLine();
             
             if(freeMenu.equals("1")) {
-            	ArrayList<JSCDTO> jscflist  = new ArrayList<JSCDTO>();
                 // JSTUDY 커뮤니티 - 자유 게시판 메뉴 -> 게시물 확인
             	System.out.println("\n===============================================");
         		System.out.println("## JSTUDY 커뮤니티 [자유 게시판] -> 게시물 확인 ## \n");
+        		ArrayList<JSCDTO> jscflist  = new ArrayList<JSCDTO>();
         		System.out.println("전체 게시물 정보 여부 : " + jscflist.isEmpty());
         		System.out.println("전체 게시물 정보 수량 : " + jscflist.size()); // 전체 게시물 정보 수
             } else if(freeMenu.equals("2")) {
-            	ArrayList<JSCDTO> jscflist  = new ArrayList<JSCDTO>();
         		// JSTUDY 커뮤니티 - 자유 게시판 메뉴 -> 게시물 글쓰기
             	System.out.println("\n===============================================");
         		System.out.println("## JSTUDY 커뮤니티 [자유 게시판] -> 게시물 글쓰기 ## \n");
@@ -64,6 +63,7 @@ public class JSCMain {
         		System.out.println(" | No." + freewriteNum++ + " | 작성자 : " + writer + " | 제목 : " + headLine + " | 작성일 : " + dateFormat.format(cal.getTime()) + "\n [ 작성 내용 ] \n" + " [ " + write + " ] ");
         		// 자유 게시판
         		JSCDTO jcof1 = new JSCDTO();
+        		ArrayList<JSCDTO> jscflist  = new ArrayList<JSCDTO>();
         		jscflist.add(new JSCDTO(jcof1.getWriteNum(), jcof1.getHeadLine(), jcof1.getWriter(), jcof1.getWriteDate(), jcof1.getWrite()));
         		// Controller만 호출.
         		JSCController jscfctrl = new JSCController();
@@ -88,14 +88,13 @@ public class JSCMain {
             studyMenu = sc.nextLine();
             
             if(studyMenu.equals("1")) {
-            	ArrayList<JSCDTO> jscslist  = new ArrayList<JSCDTO>();
                 // JSTUDY 커뮤니티 - 스터디 메뉴 -> 게시물 확인
             	System.out.println("\n===============================================");
         		System.out.println("## JSTUDY 커뮤니티 [스터디] -> 게시물 확인 ## \n");
+        		ArrayList<JSCDTO> jscslist  = new ArrayList<JSCDTO>();
         		System.out.println("전체 게시물 정보 여부 : " + jscslist.isEmpty());
         		System.out.println("전체 게시물 정보 수량 : " + jscslist.size()); // 전체 게시물 정보 수
             } else if(studyMenu.equals("2")) {
-            	ArrayList<JSCDTO> jscslist  = new ArrayList<JSCDTO>();
         		// JSTUDY 커뮤니티 - 스터디 메뉴 -> 게시물 글쓰기
             	System.out.println("\n===============================================");
         		System.out.println("## JSTUDY 커뮤니티 [스터디] -> 게시물 글쓰기 ## \n");
@@ -111,6 +110,7 @@ public class JSCMain {
         		System.out.println(" | No." + studywriteNum++ + " | 작성자 : " + writer + " | 제목 : " + headLine + " | 작성일 : " + dateFormat.format(cal.getTime()) + "\n [ 작성 내용 ] \n" + " [ " + write + " ] ");
         		// 스터디
         		JSCDTO jcos1 = new JSCDTO();
+        		ArrayList<JSCDTO> jscslist  = new ArrayList<JSCDTO>();
         		jscslist.add(new JSCDTO(jcos1.getWriteNum(), jcos1.getHeadLine(), jcos1.getWriter(), jcos1.getWriteDate(), jcos1.getWrite()));
         		// Controller만 호출.
         		JSCController jscsctrl = new JSCController();
@@ -134,17 +134,11 @@ public class JSCMain {
             qnaMenu = sc.nextLine();
             
             if(qnaMenu.equals("1")) {
-            	ArrayList<JSCDTO> jscqlist  = new ArrayList<JSCDTO>();
                 // JSTUDY 커뮤니티 - 질문 답변 메뉴 -> 게시물 확인
             	System.out.println("\n===============================================");
         		System.out.println("## JSTUDY 커뮤니티 [질문 답변] -> 게시물 확인 ## \n");
+        		ArrayList<JSCDTO> jscqlist  = new ArrayList<JSCDTO>();
         		System.out.println("전체 게시물 정보 여부 : " + jscqlist.isEmpty());
-        		jscqlist.add(new JSCDTO(6, "반갑습니다!", "홍시", "2021/06/26 22:08:22" ,"홈페이지 제작 처음입니다!"));
-        		jscqlist.add(new JSCDTO(5, "너무 좋은 사이트네요ㅎㅎ", "himan","2021/06/26 19:12:36", "저는 현직 개발자입니다!"));
-        		jscqlist.add(new JSCDTO(4, "ㅎㅇ", "cool","2021/06/26 19:08:17", "ㅎㅇㅎㅇ"));
-        		jscqlist.add(new JSCDTO(3, "여기는 뭐하는 곳이죠??", "byeman","2021/06/25 23:46:11", "처음이라 잘 부탁드려요..ㅎㅎㅎ"));
-        		jscqlist.add(new JSCDTO(2, "오 드디어 오픈이네요!", "강남살아요","2021/06/25 20:18:02", "배우러 왔습니다!"));
-        		jscqlist.add(new JSCDTO(1, "안녕하세요!", "홍길동","2021/06/25 17:11:53", "잘 부탁드려요~"));
         		System.out.println("전체 게시물 정보 수량 : " + jscqlist.size()); // 전체 게시물 정보 수
             } else if(qnaMenu.equals("2")) {
             	ArrayList<JSCDTO> jscqlist  = new ArrayList<JSCDTO>();
