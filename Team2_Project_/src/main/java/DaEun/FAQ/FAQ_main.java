786a8bc7a3d61a3034520eff3962a46187dccca0
A <A@DESKTOP-HQT62B7>
2021-07-19 17:05:01 +0900

    테스트 (다은)

--- Team2_Project_/src/main/java/DaEun/FAQ/FAQ_main.java
+++ Team2_Project_/src/main/java/DaEun/FAQ/FAQ_main.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 
 
-public class FAQ {
+public class FAQ_main {
 // 메인기능
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		FAQ_DTO dto = new FAQ_DTO("구매내역 확인하기(메인에서 입력한 내용)",
 							" 구매내역은 마이페이지로 이동하여 확인");		
@@ -15,6 +16,10 @@ public class FAQ {
 		System.out.println("-------------------------------------");
 		System.out.println("	  ** FAQ 작성하기 **");
 		System.out.println(con.flist1(dto,new Model()));
+		System.out.println("-------------------------------------");
+		System.out.println("	  ** FAQ 목록 확인하기 **");
+		
+	
 	}
 
 }
