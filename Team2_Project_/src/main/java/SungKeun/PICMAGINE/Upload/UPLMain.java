45204f664da221471bb5b49a1df5d4bbbf7796f6
Realysk <ysk2388@gmail.com>
2021-07-25 23:35:32 +0900

    (성근) 07.25 23:34PM - 프론트단 업로드 기능 추가

---
+++ Team2_Project_/src/main/java/SungKeun/PICMAGINE/Upload/UPLMain.java
@@ -0,0 +1,84 @@
+package SungKeun.PICMAGINE.Upload;
+
+import java.util.Scanner;
+
+import SungKeun.Model;
+
+public class UPLMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		UPLMain uin = new UPLMain();
+//		uin.main(args);
+		
+		Scanner sc = new Scanner(System.in);
+		UPLDTO udto = new UPLDTO();
+		
+		System.out.println("# Picmagine 게시물 업로드 #\n");
+		
+		System.out.print(" -> 제목 : ");
+		String title = sc.nextLine();
+		udto.setTitle(title);
+		
+		System.out.print(" -> 내용 : ");
+		String contents = sc.nextLine();
+		udto.setContents(contents);
+		
+		System.out.print(" -> 파일 첨부 : ");
+		String pic_file = sc.nextLine();
+		udto.setPic_file(pic_file);
+		
+		System.out.print(" -> 태그 : ");
+		String tag = sc.nextLine();
+		udto.setTag(tag);
+		
+		System.out.print("# 스토리를 추가 하시겠습니까? (Y/N) : ");
+		String story = sc.nextLine();
+		
+		if(story.equals("Y")) {
+			
+			System.out.println("# Picmagine 게시물 업로드 : 스토리 추가 #\n");
+			
+			System.out.print(" -> 스토리 제목 : ");
+			String storyName = sc.nextLine();
+			udto.setStoryname(storyName);
+			
+			System.out.print(" -> 스토리 설명 : ");
+			String storyContents = sc.nextLine();
+			udto.setStorycontents(storyContents);
+			
+			System.out.print(" -> 스토리 주제 : ");
+			String storyTitle = sc.nextLine();
+			udto.setStroytitle(storyTitle);
+			
+			System.out.print("# 게시물 등록을 완료하시겠습니까? (Y/N) : ");
+			String upChk = sc.nextLine();
+			
+			if(upChk.equals("Y")) {
+				
+				System.out.println("# 게시물 등록이 완료되었습니다! #");
+				
+				UPLController uplctrl = new UPLController();
+				System.out.println("\n# Program On #\n");
+				System.out.println(uplctrl.Uploaded(udto, new Model()));
+				
+			} else {
+				
+				System.out.println("\n 등록이 취소되었습니다. 메인으로 이동합니다.");
+				uin.main(args);
+				
+			}
+			
+		} else {
+			
+			System.out.println("# 게시물 등록이 완료되었습니다! #");
+			
+			UPLController uplctrl = new UPLController();
+			System.out.println("\n# Program On #\n");
+			System.out.println(uplctrl.Uploaded(udto, new Model())); 
+		}
+
+	}
+
+}
